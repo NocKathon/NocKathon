d97825e137ef20de781f68c6ba802f4bb1eaad17
Edo Zeif <zeifido1@gmail.com>
2021-07-26 13:28:46 +0300

    (impl) Added structs

--- backend/main.py
+++ backend/main.py
@@ -2,11 +2,22 @@ from typing import Optional
 from pydantic import BaseModel
 from fastapi import FastAPI
 import requests
+from datetime import datetime
 
 app = FastAPI()
 FRONTEND_URL = ""
 
 ALERT = {}
+
+class TeamContact(BaseModel):
+    fullname: str
+    phone_number: str
+    team_name: str
+
+class Formatted_alert(BaseModel):
+    name: str
+    description: str
+    tc: TeamContact
 
 @app.get("/")
 def read_root():
@@ -14,39 +25,27 @@ def read_root():
 
 @app.post("alerts/splunk")
 def format_splunk_alert(alert_body):
-    formatted_alert = {
-        "name": alert_body.name,
-        "description": alert_body.description,
-        "tc": get_tc()
-    }
+    formatted_alert = Formatted_alert(name=alert_body.name, description=alert_body.description, tc=get_tc())
 
     format_and_notify_for_alert(formatted_alert)
 
 @app.post("alerts/am")
 def format_am_alert(alert_body):
-    formatted_alert = {
-        "name": alert_body.name,
-        "description": alert_body.description,
-        "tc": get_tc()
-    }
+    formatted_alert = Formatted_alert(name=alert_body.name, description=alert_body.description, tc=get_tc())
 
     format_and_notify_for_alert(formatted_alert)
 
 @app.post("alerts/formatted")
 def send_alert():
-    a = ALERT
+    a = ALERT.copy()
     ALERT = {}
     return a
 
 def get_tc():
-    return "Ben 0543533078"
+    tc = TeamContact(fullname="Ben Shaver",phone_number="0543533078",team_name="IdfctsId")
+    return tc
 
 def format_and_notify_for_alert(formatted_alert):
-    ALERT[formatted_alert[name] = {
-        "name": alert_body.name,
-        "description": alert_body.description,
-        "tc": get_tc()     
-    }
+    ALERT[datetime.now("%d-%m-%Y_%H:%M:%S").strftime()] = formatted_alert.dict()
 
     requests.get(FRONTEND_URL)
-
