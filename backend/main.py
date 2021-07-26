b8a7eaf1359fd735b70f6f7a1e84f405906869ff
Edo Zeif <zeifido1@gmail.com>
2021-07-26 11:55:56 +0300

    (impl) added basic alert management

--- backend/main.py
+++ backend/main.py
@@ -1,15 +1,34 @@
 from typing import Optional
-
+from pydantic import BaseModel
 from fastapi import FastAPI
+import requests
 
 app = FastAPI()
-
+FRONTEND_URL = ""
 
 @app.get("/")
 def read_root():
     return {"Hackathon": "NocKathon"}
 
+@app.post("alerts/splunk")
+def format_splunk_alert(alert_body):
+    formatted_alert = {
+        "name": alert_body.name,
+        "description": alert_body.description,
+        "tc": get_tc()
+    }
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
+@app.post("alerts/am")
+def format_am_alert(alert_body):
+    formatted_alert = {
+        "name": alert_body.name,
+        "description": alert_body.description,
+        "tc": get_tc()
+    }
+
+def get_tc():
+    return "Ben 0543533078"
+
+def format_and_send_alert(formatted_alert):
+    requests.post(FRONTEND_URL, data=formatted_alert)
+
