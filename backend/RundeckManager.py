82e60ca4c8c145419b15eb5e797626f6bbcc4612
root <root@NocPortalProd-Team4.32uk5held1bullnncdx0pa2end.bx.internal.cloudapp.net>
2021-07-26 19:13:28 +0000

    (fix) final rundeck implementation

--- backend/RundeckManager.py
+++ backend/RundeckManager.py
@@ -9,8 +9,17 @@ class RundeckManager(Rundeck):
     def run_job(self, team_name):
         job_id = self.rd.run_job_by_name(team_name)
         execution_state = self.rd.execution_state(job_id["id"])["executionState"]
-        while execution_state == "RUNNING":
+        while execution_state == "RUNNING" or execution_state == "WAITING":
             time.sleep(2)
             execution_state = self.rd.execution_state(job_id["id"])["executionState"]
 
         return execution_state
+
+    def get_all_jobs_formatted(self):
+        formmated_jobs = []
+        all_jobs = self.rd.list_all_jobs()
+
+        for job in all_jobs:
+            formmated_jobs.append({'systemName': job['name'],'teamName': job['group']})
+
+        return formmated_jobs
--- backend/main.py
+++ backend/main.py
@@ -1,11 +1,22 @@
 from typing import Optional
 from pydantic import BaseModel
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 import requests
 from datetime import datetime
 from RundeckManager import RundeckManager
 
 app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 ## CONSTS
 FRONTEND_URL = ""
@@ -39,10 +50,12 @@ def format_splunk_alert(alert_body):
     format_and_notify_for_alert(formatted_alert)
 
 @app.post("/alerts/am")
-def format_am_alert(alert_body):
-    formatted_alert = Formatted_alert(name=alert_body.name, description=alert_body.description, tc=get_tc())
+def format_am_alert(alert_body: dict):
+    # print(alert_body)
+    # formatted_alert = Formatted_alert(name=alert_body.name, description=alert_body.description, tc=get_tc())
 
-    format_and_notify_for_alert(formatted_alert)
+    # format_and_notify_for_alert(formatted_alert)
+    ...
 
 @app.post("/alerts/formatted")
 def send_alert():
@@ -50,11 +63,16 @@ def send_alert():
     ALERT = {}
     return a
 
-@app.get("/rundeck/{team_name}")
+@app.get("/rundeck/team/{team_name}")
 def run_opening_access(team_name):
     print(team_name)
     exec_status = rundeck_manager.run_job(team_name=team_name)
+    print(exec_status)
     return exec_status
+
+@app.get("/rundeck/jobs")
+def get_jobs():
+    return rundeck_manager.get_all_jobs_formatted()
 
 def get_tc():
     tc = TeamContact(fullname="Ben Shaver",phone_number="0543533078",team_name="IdfctsId")
